a597f1965514d60be34c813030cd5c3fe5f70bdb
Sourav Kurmi <sourav.patel2021@vitstudent.ac.in>
2024-12-22 20:39:32 +0530

    modified:   React_Projects/r_r_project_navigation/src/Components/LoginForm.js

--- React_Projects/r_r_project_navigation/src/Components/LoginForm.js
+++ React_Projects/r_r_project_navigation/src/Components/LoginForm.js
@@ -25,37 +25,35 @@ const LoginForm = ({ setIslogged }) => {
     navigate("/dashboard");
   };
   return (
-      <form onSubmit={submitHandler}  className="flex flex-col w-full gap-y-2 mt-6">
+    <form
+      onSubmit={submitHandler}
+      className="flex flex-col w-full gap-y-4 mt-6"
+    >
+      <div className="flex flex-col w-full gap-y-2">
         <label htmlFor="email" className="w-full">
           <p className="text-[0.875rem] text-slate-200 mb-0.5 leading-[1.375rem]">
-            Email Address <sup className="text-red-400 ">*</sup>
+            Email Address <sup className="text-red-400">*</sup>
           </p>
         </label>
-        <br></br>
         <input
           required
           type="email"
           name="email"
           id="email"
           value={formdata.email}
-          placeholder="Enter email id "
+          placeholder="Enter email id"
           onChange={changeHandler}
           className="bg-slate-700 rounded-[0.5rem] text-slate-200 w-full p-[8px] border-b-2 "
         />
-        <br></br>
-        <label className="w-full relative ">
+      </div>
+
+      <div className="flex flex-col w-full relative gap-y-2">
+        <label htmlFor="password" className="w-full">
           <p className="text-[0.875rem] text-slate-200 mb-0.5 leading-[1.375rem]">
-            Enter Password <sup className="text-red-400 ">*</sup>
+            Enter Password <sup className="text-red-400">*</sup>
           </p>
-          <br></br>
-          <span
-            onClick={() => {
-              setShowPassword((prev) => !prev);
-            }}
-            className="absolute right-30 top-[45px] cursor-pointer "
-          >
-            {showPassword ? <AiOutlineEyeInvisible fontSize={24} fill="grey" /> : <AiOutlineEye fontSize={24} fill="grey"/>}
-          </span>
+        </label>
+        <div className="relative w-full">
           <input
             required
             type={showPassword ? "text" : "password"}
@@ -63,16 +61,36 @@ const LoginForm = ({ setIslogged }) => {
             placeholder="Enter Password"
             onChange={changeHandler}
             name="password"
-            className=" bg-slate-700 rounded-[0.5rem] text-slate-200 w-full p-[8px] border-b-2 "
+            id="password"
+            className=" bg-slate-700 rounded-[0.5rem] text-slate-200 w-full p-[8px] border-b-2"
           />
-          
-          <Link to="#">
-            <p className="text-xs mt-1 text-blue-500 max-w-max ml-auto mt-4">Forgot Password</p>
-          </Link>
-        </label>
-        <button className="bg-yellow-500 rounded-[8px] font-medium text-slate-800 px-[12px] py-[8px] mt-6">SignIn</button>
-      </form>
-  
+          <span
+            onClick={() => setShowPassword((prev) => !prev)}
+            className="absolute right-3 top-2 cursor-pointer"
+          >
+            {showPassword ? (
+              <AiOutlineEyeInvisible fontSize={24} fill="grey" />
+            ) : (
+              <AiOutlineEye fontSize={24} fill="grey" />
+            )}
+          </span>
+        </div>
+        <Link
+          to="#"
+          className="text-xs mt-1 text-blue-500 max-w-max ml-auto mt-4"
+        >
+          Forgot Password
+        </Link>
+      </div>
+      <div className="w-full">
+        <button
+          type="submit"
+          className="bg-yellow-500 w-full rounded-[8px] font-medium text-slate-800 px-[12px] py-[8px] mt-6"
+        >
+          Sign In
+        </button>
+      </div>
+    </form>
   );
 };
 
